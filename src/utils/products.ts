cc51e18b0df0b287ea76fb4f9fa23b16b05a9e58
AugustinSorel <sorelaugustin@gmail.com>
2023-01-02 14:24:20 +0000

    products added

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -1,27 +1,102 @@
 export type Product = {
   id: string;
-  text: string;
   price: number;
-  slide?: string;
+  name: string;
+  path: string;
+  images: String[];
+  coverImage: string;
+  createdAt: Date;
 };
+
+export type ProductCarousel = {
+  id: string;
+  price: number;
+  name: string;
+  path: string;
+  slide: string;
+  text: string;
+};
+
+export const productsCarousel: ProductCarousel[] = [
+  {
+    id: "791441b2-f93f-4d99-8508-b89d469d0ab4",
+    price: 49.99,
+    name: "Jade hoops",
+    path: "products/jade-hoops",
+    slide: "/slides/slide-1.png",
+    text: "Gold big hoops",
+  },
+  {
+    id: "6b2031e4-9c05-4ae6-9759-98568959e96b",
+    price: 20,
+    name: "Alice ring",
+    path: "/products/alice-ring",
+    slide: "/slides/slide-2.png",
+    text: "Pure elgence ring",
+  },
+  {
+    id: "f042c1a3-d924-4c93-b131-b26d8b753f65",
+    price: 30,
+    name: "Belle earring",
+    path: "products/belle-earring",
+    slide: "/slides/slide-3.png",
+    text: "Silver rounded erring",
+  },
+];
 
 export const products: Product[] = [
   {
-    id: "gold-big-hoops",
-    text: "Gold big hoops",
-    price: 600,
-    slide: "/slide-1.png",
+    id: "eb6c14ec-014d-4c78-8355-5dc9c44a46e5",
+    name: "lira earring",
+    price: 20,
+    path: "/products/lira-earring",
+    images: ["/products/img-1.png"],
+    coverImage: "/products/img-1.png",
+    createdAt: new Date("20022-12-01"),
   },
   {
-    id: "nice-silver-ring",
-    text: "Nice silver ring",
-    price: 899,
-    slide: "/slide-2.png",
+    id: "24db1045-3beb-41ce-9b19-548bf458529f",
+    name: "hal earrings",
+    price: 25,
+    path: "products/hal-earrings",
+    images: ["/products/img-2.png"],
+    coverImage: "/products/img-2.png",
+    createdAt: new Date("20022-12-01"),
   },
   {
-    id: "simple-elegant-earring",
-    text: "Simple elegant earring",
-    price: 299,
-    slide: "/slide-3.png",
+    id: "6f40eb48-0072-4ede-a3d3-ed020497e124",
+    name: "kaede hair pin set of 3",
+    price: 30,
+    path: "products/kaede-hair-pin-set-of-three",
+    images: ["/products/img-3.png"],
+    coverImage: "/products/img-3.png",
+    createdAt: new Date("20022-12-01"),
+  },
+  {
+    id: "03295f6f-e34f-44c6-82d9-127f5b34b9b1",
+    name: "hair pin set of 3",
+    price: 30,
+    path: "products/hair-pin-set-of-three",
+    images: ["/products/img-4.png"],
+    coverImage: "/products/img-4.png",
+    createdAt: new Date("20022-12-01"),
+  },
+  {
+    id: "c79a9833-3a7b-4bbc-afca-0564844cc2d4",
+    name: "plaine necklace",
+    price: 19,
+    path: "products/plaine-necklace",
+    images: ["/products/img-5.png"],
+    coverImage: "/products/img-5.png",
+    createdAt: new Date("20022-12-01"),
+  },
+  {
+    id: "e214cef6-f668-4ec3-a6ce-e8cd2a975cf9",
+    name: "yuki hair pin set of 3",
+    price: 29,
+    path: "/products/yuki-hair-pin-set-of-three",
+    images: ["/products/img-6.png"],
+    coverImage: "/products/img-6.png",
+    createdAt: new Date("20022-12-01"),
   },
 ];
