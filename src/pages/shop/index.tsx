98ddc5f2b446ac1fafaa47e09fa5dec35f84a1ae
AugustinSorel <sorelaugustin@gmail.com>
2023-01-04 18:32:29 +0000

    range slider component added to the shop page

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { useState } from "react";
 import DropDown from "../../components/Dropdown";
 import GridOfProducts from "../../components/GridOfProducts";
+import RangeSlider from "../../components/RangeSlider";
 import {
   maxProductPrice,
   minProductPrice,
@@ -8,92 +9,17 @@ import {
 } from "../../utils/products";
 import styles from "/src/styles/shopPageStyles.module.scss";
 
-const RangeSlider = ({
-  min,
-  max,
-  onChange,
-  value,
-}: {
-  min: number;
-  max: number;
-  value: { min: number; max: number };
-  onChange: (value: { min: number; max: number }) => void;
-}) => {
-  const [minValue, setMinValue] = useState(value ? value.min : min);
-  const [maxValue, setMaxValue] = useState(value ? value.max : max);
-  const step = 1;
-
-  useEffect(() => {
-    if (value) {
-      setMinValue(value.min);
-      setMaxValue(value.max);
-    }
-  }, [value]);
-
-  const handleMinChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const newMinVal = Math.min(+e.target.value, maxValue - step);
-    if (!value) setMinValue(newMinVal);
-    onChange({ min: newMinVal, max: maxValue });
-  };
-
-  const handleMaxChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const newMaxVal = Math.max(+e.target.value, minValue + step);
-    if (!value) setMaxValue(newMaxVal);
-    onChange({ min: minValue, max: newMaxVal });
-  };
-
-  const minPos = ((minValue - min) / (max - min)) * 100;
-  const maxPos = ((maxValue - min) / (max - min)) * 100;
-
-  return (
-    <div className={styles["slider-wrapper"]}>
-      <div className={styles["input-wrapper"]}>
-        <input
-          className={styles["input"]}
-          type="range"
-          value={minValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={handleMinChange}
-        />
-        <input
-          className={styles["input"]}
-          type="range"
-          value={maxValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={handleMaxChange}
-        />
-      </div>
-
-      <div className={styles["control-wrapper"]}>
-        <div className={styles["control"]} style={{ left: `${minPos}%` }} />
-        <div className={styles["rail"]}>
-          <div
-            className={styles["inner-rail"]}
-            style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
-          />
-        </div>
-        <div className={styles["control"]} style={{ left: `${maxPos}%` }} />
-      </div>
-    </div>
-  );
-};
-
-//TODO: create files //TODO: tab navigation for card
+//TODO: tab navigation for card
 
 const ShopPage = () => {
   const [value, setValue] = useState({
     min: minProductPrice,
     max: maxProductPrice,
   });
+
   return (
     <section className={styles["container"]}>
-      <h2 className={styles["title"]}>shop the latest</h2>
+      <h2 className={styles["title"]}>shop</h2>
       <aside className={styles["side-bar"]}>
         <div className={styles["search-container"]}>
           <input
