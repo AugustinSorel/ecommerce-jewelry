425649638396e82cc5129e6584da93a73c2cc26a
AugustinSorel <sorelaugustin@gmail.com>
2023-01-06 17:16:13 +0000

    basic toaster component added

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -42,6 +42,16 @@ const MobileMenu = () => {
   );
 };
 
+const Toaster = ({ onClose }: { onClose: () => void }) => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => onClose(), 3000);
+
+    return () => clearTimeout(timeoutId);
+  }, [onClose]);
+
+  return <h1>hello world</h1>;
+};
+
 const Header = ({
   isMobileMenuOpen,
   toggleIsMobileMenuOpen,
@@ -50,14 +60,26 @@ const Header = ({
   toggleIsMobileMenuOpen: () => void;
 }) => {
   const router = useRouter();
-  const { getNumberOfItems } = useCartStore();
+  const { getNumberOfItems, items } = useCartStore();
+
+  const [showToaster, setShowToaster] = useState(false);
+
+  const handleCloseToaster = () => {
+    setShowToaster(false);
+  };
+
+  useEffect(() => {
+    if (getNumberOfItems() > 0) {
+      setShowToaster(() => true);
+      console.log("show toaster");
+    }
+  }, [items]);
 
   return (
     <header className={styles.header}>
       <h1 className={allerta.className}>
         <Link href={"/"}>shoppe</Link>
       </h1>
-
       <nav>
         {[
           { href: "/shop", text: "shop" },
@@ -75,7 +97,6 @@ const Header = ({
           </Link>
         ))}
       </nav>
-
       <button aria-label="search">
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="m15.97 17.031c-1.479 1.238-3.384 1.985-5.461 1.985-4.697 0-8.509-3.812-8.509-8.508s3.812-8.508 8.509-8.508c4.695 0 8.508 3.812 8.508 8.508 0 2.078-.747 3.984-1.985 5.461l4.749 4.75c.146.146.219.338.219.531 0 .587-.537.75-.75.75-.192 0-.384-.073-.531-.22zm-5.461-13.53c-3.868 0-7.007 3.14-7.007 7.007s3.139 7.007 7.007 7.007c3.866 0 7.007-3.14 7.007-7.007s-3.141-7.007-7.007-7.007z" />
@@ -98,7 +119,8 @@ const Header = ({
             className={isMobileMenuOpen ? styles["open"] : ""}
           />
         </svg>
-      </button>
+      </button>{" "}
+      {showToaster && <Toaster onClose={handleCloseToaster} />}
     </header>
   );
 };
