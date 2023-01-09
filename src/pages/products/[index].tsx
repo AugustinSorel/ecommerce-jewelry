0f5129a2180437ddf75a405cb11a7233380d5edb
AugustinSorel <sorelaugustin@gmail.com>
2023-01-09 20:59:42 +0000

    product page added

---
+++ src/pages/products/[index].tsx
@@ -0,0 +1,107 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import Image from "next/image";
+import { products } from "../../utils/products";
+import styles from "src/styles/productDetails.module.scss";
+import { useState } from "react";
+import { useCartStore } from "../../store/useCart";
+import GridOfProducts from "../../components/GridOfProducts";
+
+const ProductPage = ({
+  product,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const addItem = useCartStore((state) => state.addItem);
+
+  const [quantity, setQuantity] = useState(0);
+  const incrementQuantity = () => setQuantity((prev) => prev + 1);
+  const decrementQuantity = () =>
+    setQuantity((prev) => (prev === 0 ? 0 : prev - 1));
+
+  return (
+    <>
+      <div className={styles["container"]}>
+        <Image
+          src={product.coverImage}
+          width={500}
+          height={500}
+          alt="product image"
+          priority
+        />
+
+        <div className={styles["product-details-container"]}>
+          <h2 className={styles["product-title"]}>{product.name}</h2>
+          <p className={styles["product-price"]}>$ {product.price}</p>
+          <h3 className={styles["product-description-title"]}>description</h3>
+          <p className={styles["product-description"]}>
+            {product.description}{" "}
+          </p>
+          <div className={styles["add-to-cart-container"]}>
+            <div className={styles["add-to-cart-number-picker"]}>
+              <button
+                className={styles["add-to-cart-number-picker-button"]}
+                onClick={decrementQuantity}
+              >
+                -
+              </button>
+              <p>{quantity}</p>
+              <button
+                className={styles["add-to-cart-number-picker-button"]}
+                onClick={incrementQuantity}
+              >
+                +
+              </button>
+            </div>
+            <button
+              className={styles["add-to-cart-button"]}
+              onClick={() => addItem(product.id, quantity)}
+            >
+              add to cart
+            </button>
+          </div>
+          <p className={styles["category-text"]}>
+            <strong>category: </strong>
+            {product.category}
+          </p>
+        </div>
+      </div>
+      <div className={styles["similar-items-container"]}>
+        <h2 className={styles["similar-items-title"]}>similar items</h2>
+        <GridOfProducts products={products.slice(0, 4)} />
+      </div>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: products.map((product) => ({
+      params: { index: product.id },
+    })),
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = (context) => {
+  const product = products.find(
+    (product) => product.path === `products/${(context.params ?? {}).index}`
+  );
+
+  if (!product) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      product: {
+        ...product,
+        createdAt: product.createdAt.toISOString(),
+      },
+    },
+  };
+};
+
+export default ProductPage;
