31c43a2cada41711a7ebb7c31b5c962c7b76debe
AugustinSorel <sorelaugustin@gmail.com>
2023-01-18 14:17:47 +0000

    console.log removeed

--- src/pages/products/[index].tsx
+++ src/pages/products/[index].tsx
@@ -87,8 +87,6 @@ export const getStaticProps: GetStaticProps = (context) => {
     (product) => product.path === (context.params ?? {}).index
   )!;
 
-  console.log(product, context);
-
   if (!product) {
     return {
       redirect: {
